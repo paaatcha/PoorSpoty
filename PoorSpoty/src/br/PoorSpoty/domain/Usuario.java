61aea4349d913dc5e912b9d02da9457a7011f5b3
paaatcha <pacheco.comp@gmail.com>
2015-04-30 19:06:39 -0300

    Iniciando com as classes

---
+++ PoorSpoty/src/br/PoorSpoty/domain/Usuario.java
@@ -0,0 +1,109 @@
+package br.PoorSpoty.domain;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Usuario {
+
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	private char sexo;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNasc;
+	
+	@OneToMany
+	private Set<Banda> bandas;
+	
+	@OneToMany
+	private Set<EstiloMusical> estilos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public Set<Banda> getBandas() {
+		return bandas;
+	}
+
+	public void setBandas(Set<Banda> bandas) {
+		this.bandas = bandas;
+	}
+
+	public Set<EstiloMusical> getEstilos() {
+		return estilos;
+	}
+
+	public void setEstilos(Set<EstiloMusical> estilos) {
+		this.estilos = estilos;
+	}
+	
+	
+	
+}
