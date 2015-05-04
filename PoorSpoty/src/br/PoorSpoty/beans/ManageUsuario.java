33a57551bcdc68cbb5391a30998171f9c7e8b685
patcha <patcha@patcha-PC>
2015-05-04 00:30:42 -0300

    Cadastro do usuário funcionando

--- PoorSpoty/src/br/PoorSpoty/beans/ManageUsuario.java
+++ PoorSpoty/src/br/PoorSpoty/beans/ManageUsuario.java
@@ -23,13 +23,10 @@ public class ManageUsuario implements Serializable{
 	
 	@EJB
 	UsuarioDAO usuarioDAO;
-	@EJB
-	BandaDAO bandaDAO;
-	@EJB
-	UsuarioDAO estiloMusicalDAO;
 	
-	Usuario usuario = new Usuario();
 	DataModel<Usuario> usuarios;
+	
+	Usuario usuario;
 	
 	public Usuario getUsuario() {
 		return usuario;
@@ -47,17 +44,61 @@ public class ManageUsuario implements Serializable{
 		return usuarios;		
 	}
 	
-	public String adicionar(){
-		this.usuario = new Usuario();
+	public String adicionar (){	
+		this.usuario = new Usuario ();
 		return "/manageUsuario/visualizar_usuario";
+	}
+	
+	public String salvar (){
+		try{
+			this.usuarioDAO.salvar(usuario);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "/manageUsuario/listar_usuarios";
+	}
+	
+	public void excluir(){
+		Long idUsuario = ((Usuario)this.usuarios.getRowData()).getId();
+		try{
+			this.usuarioDAO.excluir(idUsuario);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		//return "/manageUsuario/listar_usuarios";		
 	}
 	
 	public String alterar(){
 		this.usuario = (Usuario) (this.usuarios.getRowData());
 		return "/manageUsuario/visualizar_usuario";
 	}
+	
+	public String retornar (){
+		return "/manageUsuario/listar_usuarios";
+	}
+	
+	/*
+	
+	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+	public String adicionar(){
+		this.usuario = new Usuario();
+		return "/manageUsuario/visualizar_usuario";
+	}
+	
+	
 		
 	public String salvar(){
+		this.usuario = new Usuario();
 		try{
 			this.usuarioDAO.salvar(usuario);
 		}catch(Exception e){
@@ -66,18 +107,12 @@ public class ManageUsuario implements Serializable{
 		return "/manageUsuario/listar_usuarios";
 	}	
 	
-	public String excluir(){
-		Long idUsuario = ((Usuario)this.usuarios.getRowData()).getId();
-		try{
-			this.usuarioDAO.excluir(idUsuario);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return "/manageUsuario/listar_usuarios";
-	}
+	
 	
 	public String retornar(){
 		return "/manageUsuario/listar_usuarios";
 	}
+
+	*/
 
 }
